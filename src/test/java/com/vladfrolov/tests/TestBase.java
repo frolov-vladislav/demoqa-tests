f732c6465b0e3848dfb6c07c25b6e9951a54f642
frolov-vlad <shizako98@mail.ru>
2023-06-16 13:34:43 +0400

    Add load timeout

--- src/test/java/com/vladfrolov/tests/TestBase.java
+++ src/test/java/com/vladfrolov/tests/TestBase.java
@@ -2,18 +2,18 @@ package com.vladfrolov.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import  helpers.Attach;
 
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-
+        Configuration.pageLoadTimeout = 60000;
         SelenideLogger.addListener("Allure", new AllureSelenide());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
