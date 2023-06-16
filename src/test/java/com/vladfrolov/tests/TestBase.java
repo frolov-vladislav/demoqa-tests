6b4ba507142b45857ce49f197966335e85d6d3f1
frolov-vlad <shizako98@mail.ru>
2023-06-16 13:45:30 +0400

    Add maximized

--- src/test/java/com/vladfrolov/tests/TestBase.java
+++ src/test/java/com/vladfrolov/tests/TestBase.java
@@ -12,7 +12,7 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        Configuration.startMaximized = true;
         Configuration.pageLoadTimeout = 60000;
         SelenideLogger.addListener("Allure", new AllureSelenide());
 
